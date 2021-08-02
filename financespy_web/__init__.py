cef95f055fc3abf9a224863eb720491da16ea6eb
Danilo Oliveira <danilomendoncaoliveira@gmail.com>
2021-08-02 22:02:34 +0200

    Adds application factory

--- financespy_web/__init__.py
+++ financespy_web/__init__.py
@@ -1,1 +1,13 @@
 from financespy_web.transactions import transactions_blueprint
+from flask import Flask
+from financespy.account import open_account
+
+
+def create_app(config):
+    app = Flask(__name__)
+
+    account = open_account('/home/danilo/Documents/finances/bank')
+
+    app.register_blueprint(transactions_blueprint(account, __name__))
+
+    return app
